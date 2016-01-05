9cb9283662dfddae973d06d32bf5db6a231be414
gajdp9323 <gajdp9323@FHC-IC0026786.wrdsb.ca>
2016-01-05 08:56:40 -0500

    Scratches (working on bullet one right now)

--- src/Bullet/Bullet.java
+++ src/Bullet/Bullet.java
@@ -1,10 +1,17 @@
 
 package Bullet;
 
+
 /**
  *
  * @author gajdp9323
  */
 public class Bullet {
+    int nX = 300;
+    int nY = 300;
     
+    Image imgBullet = "Bullet.png";
+    double rotationRequired = Math.toRadians (45);
+    double locationX = image.getWidth() / 2;
+    double locationY = image.getHeight() / 2;    
 }
