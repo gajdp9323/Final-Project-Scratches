d9ab128367270ffa5b91bcb07d9da40629364172
gajdp9323 <gajdp9323@FHC-IC0026786.wrdsb.ca>
2016-01-06 09:32:23 -0500

    Scratches (working on bullet one right now)[think i got bullet to get drawn]

--- src/Bullet/Bullet.java
+++ src/Bullet/Bullet.java
@@ -1,17 +1,25 @@
 
 package Bullet;
 
-
+//import java.awt.Image;
+import java.awt.*;
+//import javax.imageio.ImageIO;
+//import java.io.File;
+//import java.io.IOException;
 /**
  *
  * @author gajdp9323
  */
-public class Bullet {
+public class Bullet{
     int nX = 300;
     int nY = 300;
+    //final BufferedImage imgBulletSide = ImageIO.read(new File("BulletSide.png"));
+    //final BufferedImage imgBulletUp = ImageIO.read(new File("BulletSide.png"));
+    Image imgBulletSide = Toolkit.getDefaultToolkit().createImage("BulletSide.png");
+    Image imgBulletUp = Toolkit.getDefaultToolkit().createImage("BulletUp.png");
     
-    Image imgBullet = "Bullet.png";
-    double rotationRequired = Math.toRadians (45);
-    double locationX = image.getWidth() / 2;
-    double locationY = image.getHeight() / 2;    
+    public void paint(Graphics g) {
+        g.drawImage(imgBulletSide, nX, nY, null);
+        g.drawImage(imgBulletUp, nX, nY, null);
+    }
 }
