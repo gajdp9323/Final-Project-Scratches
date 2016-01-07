767d4b014aabb25b0c9d8f013646de39e67c4e92
gajdp9323 <gajdp9323@FHC-IC0026733.wrdsb.ca>
2016-01-07 11:20:23 -0500

    working on stats

---
+++ src/Stats/PanGame.java
@@ -0,0 +1,30 @@
+package Stats;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ *
+ * @author gajdp9323
+ */
+
+public class PanGame extends JPanel // panel definition
+{
+    Image img = Toolkit.getDefaultToolkit().createImage("background.jpg");
+    public PanGame() {
+        //add(lblName); //add it to the Frame
+    }
+    public void paint(Graphics g)
+    {
+        revalidate();
+        repaint();
+        // Draws the img to the BackgroundPanel.
+        g.drawImage(img, 0, 0, null);
+    }
+
+    /*void UpdateLabel(String _sNew) {
+        sLabel = _sNew;
+        lblName.setText(sLabel);
+    }    */
+}
