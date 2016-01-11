c45f4f7cbe70b2c82bfb311531f66a56ed448bd3
gajdp9323 <gajdp9323@FHC-IC0026786.wrdsb.ca>
2016-01-11 09:05:04 -0500

    Player1 update

--- src/Player1/Player1Ship.java
+++ src/Player1/Player1Ship.java
@@ -1,29 +1,56 @@
 package Player1;
 
-
-
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.*;
+import java.awt.image.AffineTransformOp;
+import java.awt.geom.AffineTransform;
+import javax.swing.*;
+
 /**
  *
  * @author gajdp9323
+ * 
+ * ImageIcon i = new ImageIcon("src\image");
+            Image image = i.getImage();
+            int x = 0;
+            int y = 0;
+            g.drawImage(image, x, y, 100,100,null);
+ * 
  */
-public class Player1Ship {
+public class Player1Ship extends JPanel{
     Image imgPlayer1 = Toolkit.getDefaultToolkit().createImage("JavaShipP1.png");
-    double dMaxSpeed = 1, dRotateSpeed = 0.1, dShipAngle, dY, dX;
+    double dAccel, dMaxSpeed, dRotateNegativeAccel, dRotateSpeed = 0.1, dShipAngle=0, nP2X, nP2Y, dMovementSpeed = 1, dX=188, dY=188;
     Graphics g;
-    public Player1Ship(KeyEvent e) {
-        //KeyEvent event
-        int KeyCode =e.getKeyCode();
-        while (true) {
+    public Player1Ship() {
+        //KeyEvent event;
+        //while (true) {
+            System.out.println("hello");
+            int KeyCode = KeyEvent.KEY_PRESSED;
             if (dShipAngle>=360) dShipAngle-=360;
             else if (dShipAngle<0) dShipAngle+=360;
-            g.drawImage(imgPlayer1, 0, 0, null);
+            double dRotReq = Math.toRadians(dShipAngle);
+            double nP2XImg = imgPlayer1.getWidth(null)/2;
+            double nP2YImg = imgPlayer1.getHeight(null)/2;
+            AffineTransform tx = new AffineTransform();
+            tx.translate(dX+nP2XImg, dY+nP2YImg);
+            tx.rotate(dRotReq);
+            tx.translate(-dX+nP2XImg,-dY+nP2YImg);
+            AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+            //g.drawImage(imgPlayer1, (int)dX, (int)dY, null);
             if(KeyCode == KeyEvent.VK_W){
                 if (dShipAngle>=0 && dShipAngle<90) {
-                    
+                    //double dRot=90-dShipAngle;
+                    //double dXMov=(45-dRot)/90;
+                    //double dYMov=
+                    double dDegrees1 = dShipAngle;
+                    double dDegrees2 = 90-dShipAngle;
+                    double dTotalDegrees = dDegrees1 + dDegrees2;
+                    double dXMove = (dTotalDegrees/dDegrees1)*dMovementSpeed;
+                    double dYMove = (dTotalDegrees/dDegrees2)*dMovementSpeed;
+                    dX+=dXMove;
+                    dY+=dYMove;
                 }
                 else if (dShipAngle>=90 && dShipAngle<180) {
                     
@@ -50,11 +77,15 @@ public class Player1Ship {
                 }
             }
             if(KeyCode == KeyEvent.VK_A){
-           	dShipAngle += dRotateSpeed;
+           	dShipAngle -= dRotateSpeed;
             }
             if(KeyCode == KeyEvent.VK_D){
-                dShipAngle -= dRotateSpeed;
+                dShipAngle += dRotateSpeed;
             }
+            if(KeyCode == KeyEvent.VK_SPACE){
+                
+            }
+            System.out.println("P1 " + dShipAngle);
         }
     }
-}
+//}
