ee1571cfd2a6544ac2d023bf11fdfbf186e8224a
gajdp9323 <>
2016-01-11 22:29:31 -0500

    Made KeyListenerTest into a minigame where you can control two different squares and move them across the map tried to create a scoring system that goes up when both shapes move simitaniously.

--- src/KeyListenerTest/KeyListenerTest.java
+++ src/KeyListenerTest/KeyListenerTest.java
@@ -6,14 +6,15 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.Graphics;
 import java.awt.Color;
+//import java.awt.Image;
+//import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 class KeyListenerTester extends JFrame implements KeyListener {
-
     JLabel label;
-    static int nX1 = 300, nY1 = 300, nX2 = 100, nY2 = 100;
+    static int nX1 = 300, nY1 = 300, nX2 = 100, nY2 = 100, nScore = 0, nX1Temp = nX1, nX2Temp = nX2, nY1Temp = nY1, nY2Temp = nY2;
     public KeyListenerTester(String s) {
         JPanel p = new JPanel();
         label = new JLabel("Key Listener!");
@@ -33,46 +34,45 @@ class KeyListenerTester extends JFrame implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            System.out.println("Right key pressed");
-            nX1 +=1;
-            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            //System.out.println("Right key pressed");   //debug code
+            nX1 +=1; //to move right
+            //System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
         }
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            System.out.println("Left key pressed");
-            nX1 -=1;
-            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            //System.out.println("Left key pressed");
+            nX1 -=1; //to move left
+            //System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
         }
         if (e.getKeyCode() == KeyEvent.VK_UP) {
-            System.out.println("Up key pressed");
-            nY1 -=1;
-            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            //System.out.println("Up key pressed");
+            nY1 -=1; //to move up
+            //System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
         }
         if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            System.out.println("Down key pressed");
-            nY1 +=1;
-            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            //System.out.println("Down key pressed");
+            nY1 +=1; //to move down
+            //System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
         }
         if (e.getKeyCode() == KeyEvent.VK_W) {
-            System.out.println("W key pressed");
-            nY2 -=1;
-            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            //System.out.println("W key pressed");
+            nY2 -=1; //to move up
+            //System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
         }
         if (e.getKeyCode() == KeyEvent.VK_A) {
-            System.out.println("A key pressed");
-            nX2 -=1;
-            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            //System.out.println("A key pressed");
+            nX2 -=1; //to move right
+            //System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
         }
         if (e.getKeyCode() == KeyEvent.VK_S) {
-            System.out.println("S key pressed");
-            nY2 +=1;
-            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            //System.out.println("S key pressed");
+            nY2 +=1; //to move down
+            //System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
         }
         if (e.getKeyCode() == KeyEvent.VK_D) {
-            System.out.println("D key pressed");
-            nX2 +=1;
-            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            //System.out.println("D key pressed");
+            nX2 +=1; //to move left
+            //System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
         }
 
     }
@@ -81,38 +81,106 @@ class KeyListenerTester extends JFrame implements KeyListener {
     public void keyReleased(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             System.out.println("Right key Released");
+            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            System.out.println("Score: " + nScore);
+            if (nX1Temp<(nX1-5) || nX1Temp>(nX1+5) || nX2Temp<(nX2-5) || nX2Temp>(nX2+5)) {
+                nScore+=100;
+                nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2; //to shorten code since this in repeated alot
+            }
+            else nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
         }
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             System.out.println("Left key Released");
+            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            System.out.println("Score: " + nScore);
+            if (nX1Temp<(nX1-5) || nX1Temp>(nX1+5) || nX2Temp<(nX2-5) || nX2Temp>(nX2+5)) {
+                nScore+=100;
+                nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
+            }
+            else nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
         }
         if (e.getKeyCode() == KeyEvent.VK_UP) {
             System.out.println("Up key Released");
+            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            System.out.println("Score: " + nScore);
+            if (nX1Temp<(nX1-5) || nX1Temp>(nX1+5) || nX2Temp<(nX2-5) || nX2Temp>(nX2+5)) {
+                nScore+=100;
+                nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
+            }
+            else nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
         }
         if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             System.out.println("Down key Released");
+            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            System.out.println("Score: " + nScore);
+            if (nX1Temp<(nX1-5) || nX1Temp>(nX1+5) || nX2Temp<(nX2-5) || nX2Temp>(nX2+5)) {
+                nScore+=100;
+                nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
+            }
+            else nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
         }
         if (e.getKeyCode() == KeyEvent.VK_W) {
             System.out.println("W key Released");
+            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            System.out.println("Score: " + nScore);
+            if (nY1Temp<(nY1-5) || nY1Temp>(nY1+5) || nY2Temp<(nY2-5) || nY2Temp>(nY2+5)) {
+                nScore+=100;
+                nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
+            }
+            else nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
         }
         if (e.getKeyCode() == KeyEvent.VK_A) {
             System.out.println("A key Released");
+            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            System.out.println("Score: " + nScore);
+            if (nY1Temp<(nY1-5) || nY1Temp>(nY1+5) || nY2Temp<(nY2-5) || nY2Temp>(nY2+5)) {
+                nScore+=100;
+                nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
+            }
+            else nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
         }
         if (e.getKeyCode() == KeyEvent.VK_S) {
             System.out.println("S key Released");
+            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            System.out.println("Score: " + nScore);
+            if (nY1Temp<(nY1-5) || nY1Temp>(nY1+5) || nY2Temp<(nY2-5) || nY2Temp>(nY2+5)) {
+                nScore+=100;
+                nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
+            }
+            else nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
         }
         if (e.getKeyCode() == KeyEvent.VK_D) {
             System.out.println("D key Released");
+            System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
+            System.out.println("Object 2 at (" + nX2 + ", " + nY2 + ")");
+            System.out.println("Score: " + nScore);
+            if (nY1Temp<(nY1-5) || nY1Temp>(nY1+5) || nY2Temp<(nY2-5) || nY2Temp>(nY2+5)) {
+                nScore+=100;
+                nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2;
+            }
+            else nX1Temp = nX1; nX2Temp = nX2; nY1Temp = nY1; nY2Temp = nY2; 
         }
     }
     public void paint(Graphics g) {
+        //Image imgPlayer1 = Toolkit.getDefaultToolkit().createImage("JavaShipP1.png"); //Tried to import images but drawing them failed
+        //Image imgPlayer2 = Toolkit.getDefaultToolkit().createImage("JavaShipP2.png"); //comented out to hopefully use in the future
         revalidate();
         repaint();
-        g.setColor(Color.BLACK);
+        g.setColor(Color.WHITE);
         g.fillRect(0, 0, 1000, 1000);
         g.setColor(Color.RED);
         g.fillRect (nX1, nY1, 5, 5);
         g.setColor(Color.BLUE);
         g.fillRect (nX2, nY2, 5, 5);
+        //g.drawImage(imgPlayer2, nX2, nY2, null); //image was not displayed
+        //g.drawImage(imgPlayer1, nX1, nY1, null); //image was not displayed
   }
     public static void main(String[] args) {
         new KeyListenerTester("Key Listener Tester");
