c3eaa56b50cb20803cc67c2e0a9a198768f80ede
gajdp9323 <Philip@Gajdosfamily-PC>
2016-01-12 21:14:39 -0500

    images drawn trying to make a bullet appear and move across screen

--- src/KeyListenerTest/KeyListenerTest.java
+++ src/KeyListenerTest/KeyListenerTest.java
@@ -8,6 +8,7 @@ import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,8 +16,12 @@ import javax.swing.JPanel;
 class KeyListenerTester extends JFrame implements KeyListener {
     Image imgPlayer1 = Toolkit.getDefaultToolkit().createImage("Player1.png");
     Image imgPlayer2 = Toolkit.getDefaultToolkit().createImage("Player2.png");
+    Image imgBulletUp = Toolkit.getDefaultToolkit().createImage("BulletUp.png");
+    Image imgBulletSide = Toolkit.getDefaultToolkit().createImage("BulletSide.png");
     JLabel label;
     static int nX1 = 300, nY1 = 300, nX2 = 100, nY2 = 100, nScore = 0, nX1Temp = nX1, nX2Temp = nX2, nY1Temp = nY1, nY2Temp = nY2, nLastDirection1 = 1, nLastDirection2 = 1;
+    static int nX1BulletUP = nX1, nY1BulletUP = nY1, nX1BulletSIDE = nX1, nY1BulletSIDE = nY1, nX2BulletSIDE = nX2, nY2BulletSIDE = nY2, nX2BulletUP = nX2, nY2BulletUP = nY2;
+    
     public KeyListenerTester(String s) {
         JPanel p = new JPanel();
         label = new JLabel("Key Listener!");
@@ -29,13 +34,15 @@ class KeyListenerTester extends JFrame implements KeyListener {
         setVisible(true);
 
     }
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///the keyTyped code is nessessary for KeyListenerTester to work but nothing is needed inside it
     @Override
     public void keyTyped(KeyEvent e) {
     }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////
     @Override
     public void keyPressed(KeyEvent e) {
+        nX1BulletUP = nX1; nY1BulletUP = nY1; nX1BulletSIDE = nX1; nY1BulletSIDE = nY1; nX2BulletSIDE = nX2; nY2BulletSIDE = nY2; nX2BulletUP = nX2; nY2BulletUP = nY2;
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             //System.out.println("Right key pressed");   //debug code
             nLastDirection1 = 4;
@@ -62,7 +69,7 @@ class KeyListenerTester extends JFrame implements KeyListener {
         }
         if (e.getKeyCode() == KeyEvent.VK_M) {
             //System.out.println("Right key pressed");   //debug code
-            Shoot shoot = new Shoot(nX1, nY1, nLastDirection2, nX2, nY2); //to move right
+            Shoot(nX1, nY1, nLastDirection2, nX2, nY2); //to move right
             //System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
         }
         if (e.getKeyCode() == KeyEvent.VK_W) {
@@ -91,11 +98,11 @@ class KeyListenerTester extends JFrame implements KeyListener {
         }
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             //System.out.println("Right key pressed");   //debug code
-            Shoot shoot = new Shoot(nX2, nY2, nLastDirection2, nX2, nY2); //to move right
+            Shoot(nX2, nY2, nLastDirection2, nX1, nY1); //to move right
             //System.out.println("Object 1 at (" + nX1 + ", " + nY1 + ")");
         }
     }
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     @Override
     public void keyReleased(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
@@ -187,18 +194,88 @@ class KeyListenerTester extends JFrame implements KeyListener {
             else nX2Temp = nX2; 
         }
     }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
     public void paint(Graphics g) {
+        System.out.println(g); //g = sun.java2d.SunGraphics2D[font=java.awt.Font[family=Dialog,name=Dialog,style=plain,size=12],color=java.awt.Color[r=0,g=0,b=0]]
+        int nWidth1 = imgPlayer1.getWidth(rootPane)/2, nWidth2 = imgPlayer2.getWidth(rootPane)/2;
+        int nHeight1 = imgPlayer1.getHeight(rootPane)/2, nHeight2 = imgPlayer2.getHeight(rootPane)/2;
+        int nBWUp = imgBulletUp.getWidth(rootPane)/2, nBWSide = imgBulletSide.getWidth(rootPane)/2;
+        int nBHUp = imgBulletUp.getHeight(rootPane)/2, nBHSide = imgBulletSide.getHeight(rootPane)/2;
         revalidate();
         repaint();
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, 2000, 2000);
-        //g.setColor(Color.RED);    images work
-        //g.fillRect (nX1, nY1, 5, 5);
-        //g.setColor(Color.BLUE);
-        //g.fillRect (nX2, nY2, 5, 5);
-        g.drawImage(imgPlayer1, nX1-(imgPlayer1.getWidth(rootPane)/2), nY1-(imgPlayer1.getHeight(rootPane)/2), null);
-        g.drawImage(imgPlayer2, nX2-(imgPlayer2.getWidth(rootPane)/2), nY2-(imgPlayer2.getHeight(rootPane)/2), null);
+        /*g.setColor(Color.RED);    //images work now
+        g.fillRect (nX1, nY1, 5, 5);
+        g.setColor(Color.BLUE);
+        g.fillRect (nX2, nY2, 5, 5);*/
+        g.drawImage(imgBulletUp, nX1BulletUP-nBWUp, nY1BulletUP-nBHUp, null);
+        g.drawImage(imgBulletSide, nX1BulletSIDE-nBWSide, nY1BulletSIDE-nBHSide, null);
+        g.drawImage(imgPlayer1, nX1-nWidth1, nY1-nHeight1, null);
+        g.drawImage(imgBulletUp, nX2BulletUP-nBWUp, nY2BulletUP-nBHUp, null);
+        g.drawImage(imgBulletSide, nX2BulletSIDE-nBWSide, nY2BulletSIDE-nBHSide, null);
+        g.drawImage(imgPlayer2, nX2-nWidth2, nY2-nHeight2, null);
   }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void Shoot(int nX, int nY, int nDirection, int nXE, int nYE) {
+        System.out.println("Hi");
+        if (nX == nX1 && nY == nY1) {
+            if (nDirection == 1) {
+                while (nY1BulletUP>0) {
+                    nY1BulletUP-=5;
+                    System.out.println("1Not working");
+                }
+            }
+            else if (nDirection == 3) {
+                while (nY1BulletUP<1000) {
+                    nY1BulletUP+=5;
+                    System.out.println("2Not working");
+                }
+            }
+            if (nDirection == 2) {
+                while (nX1BulletSIDE<1000) {
+                    nX1BulletSIDE+=5;
+                    System.out.println("3Not working");
+                }
+            }
+            else if (nDirection == 4) {
+                while (nX1BulletSIDE>0) {
+                    nX1BulletSIDE-=5;
+                    System.out.println("4Not working");
+                }
+            }
+        }
+        else if (nX == nX2 && nY == nY2) {
+            if (nDirection == 1) {
+                while (nY2BulletUP>0) {
+                    nY2BulletUP-=5;
+                }
+            }
+            else if (nDirection == 3) {
+                while (nY2BulletUP<1000) {
+                    nY2BulletUP+=5;
+                }
+            }
+            else if (nDirection == 2) {
+                while (nX2BulletSIDE<1000) {
+                    nX2BulletSIDE+=5;
+                }
+            }
+            else if (nDirection == 4) {
+                while (nX2BulletSIDE>0) {
+                    nX2BulletSIDE-=5;
+                }
+            }
+        }
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public static void main(String[] args) {
         new KeyListenerTester("Key Listener Tester");
     }
